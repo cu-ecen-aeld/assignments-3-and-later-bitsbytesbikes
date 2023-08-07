2e3acb34d934ab7623d055ae662e1e909358c051
Robert Eichinger <robert.eichinger.re@gmail.com>
2023-08-07 20:24:19 +0200

    Removed absolute path

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,7 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-FINDER_APP_DIR=/home/baggio/repos/assignment-1-bitsbytesbikes/finder-app
+FINDER_APP_DIR=`pwd`
 
 if [ $# -lt 1 ]
 then
